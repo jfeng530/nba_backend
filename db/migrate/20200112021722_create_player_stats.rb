932496be4f3e201b102df26a00f1aceab71ffbce
github username <github email address>
2020-01-11 21:20:47 -0500

    added new migration files

---
+++ db/migrate/20200112021722_create_player_stats.rb
@@ -0,0 +1,6 @@
+class CreatePlayerStats < ActiveRecord::Migration[6.0]
+  def change
+    create_table :player_stats do |t|
+    end
+  end
+end
---
+++ db/migrate/20200112021827_create_players.rb
@@ -0,0 +1,6 @@
+class CreatePlayers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :players do |t|
+    end
+  end
+end
