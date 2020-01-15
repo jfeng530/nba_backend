4301e475d2532db6d6f08b30985e9156ad6c6663
github username <github email address>
2020-01-15 15:35:20 -0500

    cors edit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://jfeng530.github.io'
+    origins '*'
 
     resource '*',
       headers: :any,
